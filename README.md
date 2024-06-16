9ed2240eaaf99c78600d3d622e65d5609dc6d1d9
Rembqq <nikitavolkovskyi@gmail.com>
2024-06-16 11:30:04 +0300

    completed lesson1

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 _Connect **algs4.jar** to project structure_
 
 
-`javac -cp "..\libs\algs4.jar" RandomWord.java`
+`javac -cp ".\libs\algs4.jar;.\src" RandomWord.java`
+</br>
 `Get-Content .\src\animals8.txt | java -cp ".\libs\algs4.jar;.\src" RandomWord`
--- src/RandomWord.java
+++ src/RandomWord.java
@@ -5,15 +5,17 @@ import edu.princeton.cs.algs4.StdRandom;
 public class RandomWord {
     public static void main(String[] args) {
         int elem = 0;
-        String champ = "";
+        String champ = null;
+        String word;
         while(!StdIn.isEmpty())
         {
             elem++;
+            word = StdIn.readString();
 
-            if (StdRandom.bernoulli((double) 1/elem)) {
-                champ = StdIn.readString();
+            if(StdRandom.bernoulli(1.0/elem))
+            {
+                champ = word;
             }
-
         }
 
         StdOut.println(champ);
