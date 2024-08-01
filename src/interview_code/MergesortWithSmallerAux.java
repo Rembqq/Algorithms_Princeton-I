58407d751ee0e4666cab559f1575a77d4e0314eb
Rembqq <nikitavolkovskyi@gmail.com>
2024-08-01 09:58:18 +0300

    mergesort example

---
+++ src/interview_code/MergesortWithSmallerAux.java
@@ -0,0 +1,45 @@
+package interview_code;
+
+public class MergesortWithSmallerAux {
+    public static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+        for(int k = lo; k < hi; ++k) {
+            aux[k] = a[k];
+        }
+        int i = lo, j = mid+1;
+        for(int k = lo; k < hi; ++k) {
+            if(i > mid) a[k] = aux[j++];
+            else if(j > hi) a[k] = aux[i++];
+            else if(less(aux, j, i)) a[k] = aux[j++];
+            else a[k] = aux[i++];
+        }
+    }
+
+    private static boolean less(Comparable[] aux, int i, int j) {
+        return aux[i].compareTo(aux[j]) < 0;
+    }
+    private void swap() {
+
+    }
+
+
+    private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+        if(lo >= hi) {
+            return;
+        }
+        int mid = lo + ((hi-lo) / 2);
+
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid+1, hi);
+        merge(a, aux, lo, mid, hi);
+    }
+    public static void sort(Comparable[] a) {
+        Comparable[] aux = new Comparable[a.length];
+        sort(a, aux, 0, a.length - 1);
+    }
+
+    public static void main(String[] args) {
+
+        MergesortWithSmallerAux.sort();
+
+    }
+}
