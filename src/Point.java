3ff3ff19954a3544958e08815302c4f2b2768f8d
Rembqq <nikitavolkovskyi@gmail.com>
2024-08-10 22:20:23 +0300

    Point with slopes

--- src/Point.java
+++ src/Point.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.StdDraw;
+
 import java.util.Comparator;
 
 public class Point implements Comparable<Point> {
@@ -9,14 +11,14 @@ public class Point implements Comparable<Point> {
         this.y = y;
     }
     public void draw(Point that) {
-
+        StdDraw.point(x, y);
     }
 
     public void drawTo(Point that) {
-
+        StdDraw.line(this.x, this.y, that.x, that.y);
     }
     public String toString(){
-        return "";
+        return "(" + x + ", " + y + ")";
     }
 
     public int compareTo(Point that) {
@@ -31,21 +33,25 @@ public class Point implements Comparable<Point> {
     }
     public double slopeTo(Point that) {
         if(this.y == that.y && this.x == that.x) {
-            return -1 / 0.0;
+            return Double.NEGATIVE_INFINITY;
         } else if(this.x == that.x) {
-            return 1 / 0.0;
+            return Double.POSITIVE_INFINITY;
         }
         else if(this.y == that.y) {
-            return 0;
+            return +0.0;
         } else {
             return (double)(that.y - this.y) / (that.x - this.x);
         }
     }
     public Comparator<Point> slopeOrder() {
-
+        return new SlopeOrderComparator();
     }
 
-    public static void main(String[] args) {
-
+    private class SlopeOrderComparator implements Comparator<Point>{
+        public int compare(Point p1, Point p2) {
+            double slope1 = slopeTo(p1);
+            double slope2 = slopeTo(p2);
+            return Double.compare(slope1, slope2);
+        }
     }
 }
